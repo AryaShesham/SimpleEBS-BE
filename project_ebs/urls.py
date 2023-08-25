4f54bc43945ddb59506f1770d13d9b6d88a6bd78
Arya-Shesham <Arya@riverus.in>
2023-08-25 15:11:36 +0530

    added swagger docs

--- project_ebs/urls.py
+++ project_ebs/urls.py
@@ -16,8 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Simple Event Booking System APIs",
+      default_version='v1',
+      description="This is a collection of all the APIs provided in the Simple Event Booking System Project.",
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('swagger(<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path("api/v1/", include("ebs_app.urls")),
 ]
